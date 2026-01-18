c5164b7fbfea93e8887b16d932699008260d2cbe
jasur <jasfayz@gmail.com>
2026-01-18 21:40:48 +0500

    Add hume readably file size

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -16,7 +16,9 @@ class ImageResource extends JsonResource
             'raw_hash' => $this->resource->raw_hash,
             'hash' => $this->resource->hash,
             'size' => $this->resource->size,
+            'size_formatted' => $this->resource->human_readable_size,
             'original_size' => $this->resource->original_size,
+            'original_size_formatted' => $this->resource->original_human_readable_size,
             'url' => Storage::disk('s3')
                 ->temporaryUrl($this->resource->original_path ?? $this->resource->path, now()->addMinutes(5)),
         ];
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\ImageStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,4 +30,34 @@ class Image extends Model
     protected $casts = [
         'status' => ImageStatus::class,
     ];
+
+    public function humanReadableSize(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->formatBytes($this->size),
+        );
+    }
+
+    public function originalHumanReadableSize(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->formatBytes($this->original_size),
+        );
+    }
+
+    private function formatBytes($bytes, $precision = 2): string
+    {
+        if ($bytes === null) {
+            return '0 B';
+        }
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
 }
